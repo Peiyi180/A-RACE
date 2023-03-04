eaf0676e85b6077c407560cfef415aae5910a0b5
Peiyi Jiang <peiyi180@gmail.com>
2023-03-04 00:48:33 -0400

    create adjusted version

---
+++ adjusted/README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0cf47eb2f30357374b8f373b71b185a26d25af35cc3a949f369cd29b716f8e74
+size 1117
---
+++ adjusted/model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f59991badc39449d7c01910100598311d350c1d202e6242b8ca8774c5809898
+size 10899
