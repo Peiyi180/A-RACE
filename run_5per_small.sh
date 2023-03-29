6b87aca21ef15b217ac57ee8d490ff16dbd0afca
Peiyi Jiang <py812362@dal.ca>
2023-03-29 01:24:09 -0300

    finalize script for training codet5_gpt2 with normal 5% sample data

--- run_5per_small.sh
+++ run_5per_small.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28e495d46d772379cc595a5ee8807e85811ddead77292458d950035691b18f6c
-size 3612
+oid sha256:37c261b5f3294675acb76f0f15025eb63dbbdca935c975947e2a6abd6d4aec81
+size 3608
