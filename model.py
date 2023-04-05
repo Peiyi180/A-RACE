4bb8a44abeb71b6f204169dbaabb49450b4033ed
Arthur-ca <ch243852@gmail.com>
2023-04-05 16:46:19 -0300

    clean unuseful  comment and revised comment

--- model.py
+++ model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c6116c7f52bdbc01325d73d2678dc436ae95fcb4f304dafb10b037527b7ad9a
-size 14853
+oid sha256:0112638e1534c7badf5c443c0596ffe5a03bc83bf30e03bf4c4dfa810427e5e6
+size 12225
--- unixcoder.py
+++ unixcoder.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b317b0ba63e4ea46b3547cc4a4ecca1e7b5d90e8c201e641c67682adbdfc4ad0
-size 11487
+oid sha256:0e88d08a77ee11c8e7816b84d6f7c3c5f5825055250c31e9a1620eb72a469247
+size 10529
