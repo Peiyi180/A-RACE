950838aa4e11f1746acef85507e2bf5ba9ea42a3
Peiyi Jiang <py812362@dal.ca>
2023-04-07 00:34:00 -0300

    remove unrelated files

--- run.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bb20f4e2dc556d979e8e5a81886ea001fdb71523202f3546f6c42d3772e5158d
-size 3200
--- run_small.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:24c3285fcda7193996241150c0feae58a5de778e6479afd02c928c54f8ef8794
-size 3279
