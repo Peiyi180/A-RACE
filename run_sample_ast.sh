c375396dbc0c7e639a0372b3a1e7435e82605682
Peiyi Jiang <py812362@dal.ca>
2023-04-07 01:19:43 -0300

    reduce batch size to 2

--- run_sample_ast.sh
+++ run_sample_ast.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae73cf564e7d76970688ad807fa197c566a3a91d0b99ff41f902937b4ad9ece8
+oid sha256:bb9acdd137e2ec9d52ddc3b84f5ce6e79dfcdfb99a655650f05e23dbfa5d5944
 size 3419
