6427b439de5809f2d27644571b17192578672e11
Peiyi Jiang <py812362@dal.ca>
2023-04-08 14:40:24 -0300

    remove unused files

--- run_5per_small.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:37c261b5f3294675acb76f0f15025eb63dbbdca935c975947e2a6abd6d4aec81
-size 3608
--- run_small.sh
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:08284460ff2d16a42b0b3de910416a094118bd0ecfa33b25ae86ace36a556a71
-size 3231
