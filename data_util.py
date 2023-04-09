eadf648931827538d045207fc108471261705b4a
Haoyi Zhang <hy411856@dal.ca>
2023-04-09 16:52:42 -0300

    update the load_single_commit function to remove the temp file

--- data_util.py
+++ data_util.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:935d25aee8348ec505c44a6b81bf6e97ee00e04b687d24f4bb174c5700854467
-size 14612
+oid sha256:aae584aa58b5d9649c791335d07c5db1edac82d2b7ac88c1b34724d683ded4a4
+size 14670
