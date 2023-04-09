bd92972c132eb3bc35ae6a8f952cb81a1675a060
Peiyi Jiang <py812362@dal.ca>
2023-04-09 19:16:43 -0300

    print demo

--- demo/run_gpt2.py
+++ demo/run_gpt2.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd332039646d0cf2825a49d62b4864ec63e636094deaf7ed7606ce00d3d04daf
-size 28038
+oid sha256:576f5d674bb63c6cedd9d6fe0b1111ba78ec4bc4252c597a94a483dbe8c9ed86
+size 28330
