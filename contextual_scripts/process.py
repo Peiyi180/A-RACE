a57b729920fd2e0edf54851e031863a5c2bcdf9a
Haoyi Zhang <hy411856@dal.ca>
2023-04-09 16:50:17 -0300

    add the scripts used for getting the contextual medits

---
+++ contextual_scripts/process.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:856c9b059179562dae92762d6728698bed941f11112aa9a4579e6a8eb38ac233
+size 3580
---
+++ contextual_scripts/run.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:04cbdaad5a860197491cdd77304b365e0a52f82c70f0783577022484055e1b29
+size 255
