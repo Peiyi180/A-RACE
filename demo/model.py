1b20e072438ceb4bafd9eb426776da0e5f9bef0d
Peiyi Jiang <py812362@dal.ca>
2023-04-09 20:03:17 -0300

    update go demo

--- demo/model.py
+++ demo/model.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5973df9d91166ce8b8facf8e752d7c418ba9383feb3b37999236ebfbac267e73
-size 12281
+oid sha256:bf0220723d6e4c7d859c24c3c35fa046b10913439f29966fb5497b9f4852cacd
+size 11102
--- demo/run.py
+++ demo/run.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a281e27381315b7af97d74c95140e41ad207e2d11febb91ba269c9e9b04a342c
-size 28392
+oid sha256:8937e9f4b165f0f706942bad50f01a3734bde60e0b625ec7c04ddfd76520af03
+size 28397
