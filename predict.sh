430e752d28626d105336edbb92861713dc0da9af
Peiyi Jiang <py812362@dal.ca>
2023-04-09 09:00:13 -0300

    upload ecmg status

--- predict.sh
+++ predict.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76f2635d8e365161c6929683b6764f5848d49fd84747b1c8cddcbf1642b4cbb0
-size 3407
+oid sha256:c7e793cb775a5d635f7bd82a2ce1aac75cb3b30dce2cf5312d41cb727cebf9ab
+size 3443
